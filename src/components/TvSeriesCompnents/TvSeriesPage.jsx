c0eed4cd9c7b5efa8bc78a940011b557ae038c4b
Stanley Meruo <144041200+stanley-meruo@users.noreply.github.com>
2025-08-20 19:57:14 +0100

    Fix case-sensitive filename for TvSeriesPage

---
+++ src/components/TvSeriesCompnents/TvSeriesPage.jsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react";
+import Card from "../Card";
+import { FaSpinner } from "react-icons/fa6";
+import { getAiringTV, getOnTheAirTV, getPopularTV, getTopRatedTV } from "../../services/tvseriesApi";
+import { Outlet } from "react-router-dom";
+
+
+
+const TvSeriesPage = () => {
+  const [popularTV, setPopularTV] = useState([]);
+  const [airingTV, setAiringTV] = useState([]);
+  const [onTheAirTV, setOnTheAirTV] = useState([]);
+   const [topRatedTV, setTopRatedTV] = useState([]);
+  const [loading, setLoading] = useState(true)
+  
+
+  useEffect(() => {
+  
+    const fetchData = async () => {
+      const delay = new Promise((resolve) => setTimeout(resolve, 5000));
+      const popularSeries = await getPopularTV();
+      const airingToday = await getAiringTV();
+      const onTheAir = await getOnTheAirTV();
+      const topRatedSeries = await getTopRatedTV();
+      delay;
+      setPopularTV(popularSeries);
+      setAiringTV(airingToday);
+      setOnTheAirTV(onTheAir);
+      setTopRatedTV(topRatedSeries)
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+      return (
+        <div className="flex justify-center items-center gap-4 h-screen bg-neutral-800">
+          <FaSpinner className="size-8 animate-spin text-red-600 sm:size-10 lg:size-12" />
+          <p className="font-semibold text-gray-400 sm:text-lg lg:text-xl">
+            Loading content...
+          </p>
+        </div>
+      );
+    }
+
+
+  return (
+    <section className="bg-neutral-800 py-12 px-5 xs:px-6 sm:px-8 md:px-10 lg:px-16 lg:py-16 xl:px-20 xxl:py-20 xxl:px-28">
+      <div className="space-y-5 md:space-y-8 xmd:space-y-16">
+        <div className="">
+          <div className="flex items-center justify-between text-white gap-4 md:mb-2 lg:mb-4">
+            <h2 className="text-xl font-bold uppercase md:text-2xl xl:text-3xl">
+              Popular Series
+            </h2>
+          </div>
+          <div className="relative mb-4 md:mb-6 lg:mb-8">
+            <div className="h-0.5 w-full bg-neutral-500 mt-2"></div>
+            <div className="absolute top-0 h-0.5 w-1/5 bg-red-500"></div>
+          </div>
+          <div className="flex gap-4 overflow-x-auto py-4 scrollbar-hide xmd:grid xmd:grid-cols-4 xmd:gap-6 lg:grid-cols-5">
+            {popularTV.map((item) => (
+              <Card key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+
+        <div className="">
+          <div className="flex items-center justify-between text-white gap-4 md:mb-2 lg:mb-4">
+            <h2 className="text-xl font-bold uppercase md:text-2xl xl:text-3xl">
+              Airing Today
+            </h2>
+          </div>
+          <div className="relative mb-4 md:mb-6 lg:mb-8">
+            <div className="h-0.5 w-full bg-neutral-500 mt-2"></div>
+            <div className="absolute top-0 h-0.5 w-1/5 bg-red-500"></div>
+          </div>
+          <div className="flex gap-4 overflow-x-auto py-4 scrollbar-hide xmd:grid xmd:grid-cols-4 xmd:gap-6 lg:grid-cols-5">
+            {airingTV.map((item) => (
+              <Card key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+
+        <div className="">
+          <div className="flex items-center justify-between text-white gap-4 md:mb-2 lg:mb-4">
+            <h2 className="text-xl font-bold uppercase md:text-2xl xl:text-3xl">
+              On The Air
+            </h2>
+          </div>
+          <div className="relative mb-4 md:mb-6 lg:mb-8">
+            <div className="h-0.5 w-full bg-neutral-500 mt-2"></div>
+            <div className="absolute top-0 h-0.5 w-1/5 bg-red-500"></div>
+          </div>
+          <div className="flex gap-4 overflow-x-auto py-4 scrollbar-hide xmd:grid xmd:grid-cols-4 xmd:gap-6 lg:grid-cols-5">
+            {onTheAirTV.map((item) => (
+              <Card key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+
+        <div className="">
+          <div className="flex items-center justify-between text-white gap-4 md:mb-2 lg:mb-4">
+            <h2 className="text-xl font-bold uppercase md:text-2xl xl:text-3xl">
+              ⭐ Top Rated Series
+            </h2>
+          </div>
+          <div className="relative mb-4 md:mb-6 lg:mb-8">
+            <div className="h-0.5 w-full bg-neutral-500 mt-2"></div>
+            <div className="absolute top-0 h-0.5 w-1/5 bg-red-500"></div>
+          </div>
+          <div className="flex gap-4 overflow-x-auto py-4 scrollbar-hide xmd:grid xmd:grid-cols-4 xmd:gap-6 lg:grid-cols-5">
+            {topRatedTV.map((item) => (
+              <Card key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </section>
+  );
+};
+
+export default TvSeriesPage;
